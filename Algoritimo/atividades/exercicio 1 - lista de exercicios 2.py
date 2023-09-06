2899ea1c2df1d77239abc73384a1ed9b94912a0c
Bruno Silva <brunosilvacontato2018@gmail.com>
2023-09-06 19:52:07 -0300

    git pull

---
+++ Algoritimo/atividades/exercicio 1 - lista de exercicios 2.py
@@ -0,0 +1,8 @@
+soma = 0
+print("Digite um numero")
+val1 = int(input())
+while val1 != 0:
+    soma = soma + val1
+    print("Digite um numero")
+    val1 = int(input())
+print("A soma eh:" + str(soma))
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # IFBA
 ## Repositório designado para as atividades e projetos desenvolvidos no curso técnico de informatica
 
-https://portal.ifba.edu.br/banner/banner-capa/Logon.jpg
+
+
