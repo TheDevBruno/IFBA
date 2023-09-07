1727f9593f69077ccad8f0208452177b694af88a
Bruno Silva <brunosilvacontato2018@gmail.com>
2023-09-06 21:25:47 -0300

    git pull

---
+++ Algoritimo/atividades/lista de exerciicos 2/exercicio 3 - lista de exercicios 2.py
@@ -0,0 +1,11 @@
+﻿cont = 0
+soma = 0
+print("Digite um valor")
+x = int(input())
+while x != 0:
+    soma = soma + x
+    cont = cont + 1
+    print("Digite outro valor")
+    x = int(input())
+soma = float(soma) / cont
+print("A média é: " + str(soma))
