8a5e9afd5b9276cccdb630eae4b8f82a3722da8a
Bruno Silva <brunosilvacontato2018@gmail.com>
2023-09-16 22:53:31 -0300

    to perdido

---
+++ Algoritimo/atividades/Lista de exercicios 3/atividade 5 - lista de exercicios 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float num1 = 5.0, num2 = 8.0, num3 = 9.0, media;
+
+    media = (num1 + num2 + num3) / 3;
+
+    printf("A media eh: %f", media);
+
+
+return 0;
+}
